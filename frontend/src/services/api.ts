49e2b783bcd29d04a15ad5f25f21cbaacae477ac
mohammad-salman0 <salmanmohammad339@gmail.com>
2026-07-09 15:14:02 +0530

    fixing frontend

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,36 +1,17 @@
-import axios
- from "axios";
+import axios from "axios";
 
-const api =
- axios.create({
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL + "/api",
+});
 
-  baseURL:
-   "http://localhost:5000/api",
-
- });
-
-
-api.interceptors.request.use(
-
- (config) => {
-
-  const token =
-   localStorage.getItem(
-    "token"
-   );
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
 
   if (token) {
-
-   config.headers.Authorization =
-
-    `Bearer ${token}`;
-
+    config.headers.Authorization = `Bearer ${token}`;
   }
 
   return config;
-
- }
-
-);
+});
 
 export default api;
